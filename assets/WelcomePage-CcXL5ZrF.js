d8c8eaedbf7f5bc665d2a48a633906fd4414c9bf
Mariia2308 <Mariia2308@users.noreply.github.com>
2024-05-13 11:33:54 +0000

    deploy: 96431c42882464f4a1233234685c33a63265dea6

---
+++ assets/WelcomePage-CcXL5ZrF.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-CB02xlL5.js";const o="_welcomer_9so4o_1",s={welcomer:o},c=()=>e.jsx("div",{className:s.welcomer,children:"WELCOME TO PHONEBOOK"}),t=()=>e.jsx("div",{children:e.jsx(c,{})});export{t as default};
+//# sourceMappingURL=WelcomePage-CcXL5ZrF.js.map
