75c9b004a166e6ee9376beba2c82b6abb99a36b9
Mariia2308 <Mariia2308@users.noreply.github.com>
2024-08-26 11:05:38 +0000

    deploy: c0c1917a3f43c89b727f37fa5aca9e0d7cdbed85

---
+++ assets/WelcomePage-CjMCguSu.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-bmas-8FJ.js";const o="_welcomer_9so4o_1",s={welcomer:o},c=()=>e.jsx("div",{className:s.welcomer,children:"WELCOME TO PHONEBOOK"}),t=()=>e.jsx("div",{children:e.jsx(c,{})});export{t as default};
+//# sourceMappingURL=WelcomePage-CjMCguSu.js.map
