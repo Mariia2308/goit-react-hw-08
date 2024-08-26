dd8479d7a9d93a78319715e9b88b17e962cd5a29
Mariia2308 <136304734+Mariia2308@users.noreply.github.com>
2024-08-26 15:29:56 +0300

    allala

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/goit-react-hw-08/",
+    base: "./goit-react-hw-08/",
 plugins: [react()],
 build: {
 sourcemap: true,
